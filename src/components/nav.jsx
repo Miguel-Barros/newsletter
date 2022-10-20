aa579c466ccce422e5069195a26387575b9aaef2
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-10-20 18:49:03 -0300

    Final

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,17 @@
 import styles from './styles/Nav.module.css'
 
 export default function Nav() {
-    return(
+    return (
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <ul>
-                    <li>Index</li>
-                    <li>About</li>
-                    <li>Sign In</li>
+                    <span>
+                        <li>Index</li>
+                        <li>About</li>
+                    </span>
+                    <span>
+                        <li>Sign In</li>
+                    </span>
                 </ul>
             </nav>
         </header>
