fa743223d91bfc5c63a94cbbd329ce3d34b28d8a
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-10-20 18:21:31 -0300

    a

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from './styles/Home.module.css'
 import Nav from '../components/nav'
 
 export default function Home() {
