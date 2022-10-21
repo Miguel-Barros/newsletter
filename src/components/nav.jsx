3bd5b51065ec2cb82fefde7afac71faed5a9831b
Replit user <>
2022-10-21 17:52:28 +0000

    Finish

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,19 +1,17 @@
 import styles from './styles/Nav.module.css'
 
 export default function Nav() {
-    return (
-        <header className={styles.header}>
-            <nav className={styles.nav}>
-                <ul>
-                    <span>
-                        <li>Index</li>
-                        <li>About</li>
-                    </span>
-                    <span>
-                        <li>Sign In</li>
-                    </span>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul>
+          <span>
+          </span>
+          <span>
+            <li>Sign In</li>
+          </span>
+        </ul>
+      </nav>
+    </header>
+  )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,51 +1,58 @@
 import Head from 'next/head'
 import styles from './styles/Home.module.css'
 import Nav from '../components/nav'
-
-import axios from 'axios'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function Home() {
 
   const [email, setEmail] = useState('')
   const [tel, setCel] = useState('')
-  const [response, setResponse] = useState('')
+  const inputEl = useRef(null);
+  const [message, setMessage] = useState('');
 
-  function checkSendman() {
-    if (email) return { email }
-    if (tel) return { tel }
-    if (email && tel) return { tel }
-    if (!email) return {}
-    if (!tel) return {}
-  }
+  const subscribe = async (e) => {
+    const res = await fetch('/api/subscribe', {
+      body: JSON.stringify({
+        email: inputEl.current.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    });
 
-  const subscribe = async () => {
-    try {
-      await axios.post('./api/newsletter', { email })
-      setResponse('Sucess')
-      return alert(response)
-    } catch (error) {
-      setResponse('Fail')
-      alert(error)
-      return error.response.data.error
+    const { error } = await res.json();
+
+    if (error) {
+      setMessage(error);
+      return;
     }
-  }
+    setEmail('')
+    inputEl.current.value = '';
+    setMessage('Parabéns! 🎉 você se inscreveu com sucesso em nossa NewsLetter.')
+  };
+
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>TCC SPEED RUN</title>
+        <title>Viver - A ferramenta de axulio perfeita para sua manhã</title>
       </Head>
       <Nav />
       <main className={styles.main}>
         <span className={styles.left}>
-          <h1>Bem vindo ao seu Jornal Habitual favorito</h1>
-          <p>Tenha as ultimas noticias de mongaguá na palma de sua mão</p>
-          <span className={styles.inputs}>
-            <input type="email" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)} value={email} />
-            <input type="tel" placeholder="Digite seu telefone" maxLength={11} onChange={(e) => setCel(e.target.value)} value={tel} />
-          </span>
-          <button onClick={() => subscribe()}>Inscrevera</button>
+          <h1>Seja bem vindo ao Viver</h1>
+          <p>tornando sua manhã mais importante para o seu dia!</p>
+          <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+            <span className={styles.inputs}>
+              <input required ref={inputEl} type="email" name="email" id="email-input" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)} value={email} />
+              <input type="tel" placeholder="Digite seu telefone" maxLength={11} onChange={(e) => setCel(e.target.value)} value={tel} />
+            </span>
+            <button type="submit" onClick={() => subscribe()}>Inscrever</button>
+          </form>
+          <p className={styles.confirm}>
+            {message ? message : null}
+          </p>
         </span>
         <span className={styles.right}>
           <img className={styles.ilus} src="./assets/Send-Gift.svg" alt="ilustration" />
